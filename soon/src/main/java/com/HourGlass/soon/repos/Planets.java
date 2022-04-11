061dd044d813fab5c2edd50513f977c7bbe927bc
Cooks-Johns <cooks-johns@gmail.com>
2022-04-11 15:48:16 -0500

    testing angular with spring and breaking everything lol

--- soon/src/main/java/com/HourGlass/soon/repos/Planets.java
+++ soon/src/main/java/com/HourGlass/soon/repos/Planets.java
@@ -1,10 +1,34 @@
 package com.HourGlass.soon.repos;
 
 
-import org.springframework.boot.autoconfigure.domain.EntityScan;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+// TODO - look into why this is breaking everything
 //@Entity
 public class Planets {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private final String name;
+    private final String location;
+
+
+    public Planets(String name, String location) {
+        this.name = name;
+        this.location = location;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
 
 
 
